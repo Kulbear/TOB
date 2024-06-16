73f59618b302b2ac8266b3df716aff76f7fc961c
Kulbear <jiyang.py@gmail.com>
2024-06-16 15:37:40 -0700

    Add /quest command template

---
+++ commands/utility/quest.js
@@ -0,0 +1,36 @@
+const {
+    SlashCommandBuilder,
+} = require('discord.js');
+
+const { buildQuestPublishModal } = require('../../utility/modalUtils.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('quest')
+        .setDescription('Quest related options...')
+    // Subcommand for general users
+        .addStringOption(option =>
+            option.setName('ops')
+                .setDescription('[General] Publish a quest...')
+                .setRequired(true)
+                .addChoices(
+                    { name:'publish', value:'publish' },
+                    { name:'review', value:'review' },
+                    { name:'complete', value:'complete' },
+                    { name:'list', value:'list' },
+                )),
+    async execute(interaction, supabase) {
+        if (interaction.options.getString('ops') === 'publish') {
+            console.debug('[DEBUG] "/quest publish" command received.');
+        }
+        if (interaction.options.getString('ops') === 'review') {
+            console.debug('[DEBUG] "/quest review" command received.');
+        }
+        if (interaction.options.getString('ops') === 'complete') {
+            console.debug('[DEBUG] "/quest complete" command received.');
+        }
+        if (interaction.options.getString('ops') === 'list') {
+            console.debug('[DEBUG] "/quest list" command received.');
+        }
+    },
+};
