4da0dd40128763329580df8e16c9fccef40408a3
Kulbear <jiyang.py@gmail.com>
2024-06-25 17:10:09 -0700

    Fix deploy script with updated config variable names

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,5 +1,5 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+const { clientId, guildId, discordBotToken } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 
@@ -26,7 +26,7 @@ for (const folder of commandFolders) {
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST().setToken(token);
+const rest = new REST().setToken(discordBotToken);
 
 // and deploy your commands!
 (async () => {
@@ -35,8 +35,8 @@ const rest = new REST().setToken(token);
 
         // The put method is used to fully refresh all commands in the guild with the current set
         const data = await rest.put(
-            Routes.applicationGuildCommands(clientId, guildId),
-            // Routes.applicationCommands(clientId),
+            // Routes.applicationGuildCommands(clientId, guildId),
+            Routes.applicationCommands(clientId),
             { body: commands },
         );
 
